e1d3dc79b179df75a114721c1bca844636dbe67f
Rémi Campistron <remi.campistron@gmail.com>
2026-03-30 15:51:41 +0200

    Exercice 2 : affichage des catégories

--- src/main/java/fr/ldnr/SpringStockMvc/web/ArticleController.java
+++ src/main/java/fr/ldnr/SpringStockMvc/web/ArticleController.java
@@ -1,8 +1,11 @@
 package fr.ldnr.SpringStockMvc.web;
 
 import fr.ldnr.SpringStockMvc.entities.Article;
+import fr.ldnr.SpringStockMvc.entities.Category;
 import fr.ldnr.SpringStockMvc.repositories.ArticleRepository;
+import fr.ldnr.SpringStockMvc.repositories.CategoryRepository;
 import jakarta.validation.Valid;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -18,14 +21,17 @@ public class ArticleController {
     @Autowired
     ArticleRepository articleRepository;
 
+    @Autowired
+    CategoryRepository categoryRepository;
+
     //@RequestMappingValue(value="/index" , method=RequestMethod.GET)
     @GetMapping("/index")
     public String index(Model model, @RequestParam(name="page",defaultValue = "0") int page,
                                      @RequestParam(name="keyword", defaultValue = "") String kw){
       Page<Article> articles = articleRepository.findByDescriptionContains(kw,PageRequest.of(page,5));
-
+      List<Category> categories = categoryRepository.findAll();
       model.addAttribute("listArticle",articles.getContent());
-
+      model.addAttribute("listCategories",categories);
       model.addAttribute("pages", new int[articles.getTotalPages()]);
 
       model.addAttribute("currentPage",page);
