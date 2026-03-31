3a8deac40c9995474f718ed3b1a643cfd2003418
Rémi Campistron <remi.campistron@gmail.com>
2026-03-31 15:54:42 +0200

    Exercice 4 - correction typo

---
+++ src/test/java/fr/ldnr/SpringStockMvc/SpringStockMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ldnr.SpringStockMvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStockMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
