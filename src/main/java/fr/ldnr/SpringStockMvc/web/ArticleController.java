974cedbe2642e8c998443880a12a95b27e132374
Rémi Campistron <remi.campistron@gmail.com>
2026-04-01 08:57:15 +0200

    Faire en sorte que la page d'accueil soit appelée quand on visite la racine du site - fixes #3

--- src/main/java/fr/ldnr/SpringStockMvc/web/ArticleController.java
+++ src/main/java/fr/ldnr/SpringStockMvc/web/ArticleController.java
@@ -1,23 +1,18 @@
 package fr.ldnr.SpringStockMvc.web;
 
 import fr.ldnr.SpringStockMvc.entities.Article;
-import fr.ldnr.SpringStockMvc.entities.Cart;
-import fr.ldnr.SpringStockMvc.entities.CartItem;
 import fr.ldnr.SpringStockMvc.entities.Category;
-import fr.ldnr.SpringStockMvc.entities.User;
 import fr.ldnr.SpringStockMvc.repositories.ArticleRepository;
+import fr.ldnr.SpringStockMvc.repositories.CartItemRepository;
 import fr.ldnr.SpringStockMvc.repositories.CartRepository;
 import fr.ldnr.SpringStockMvc.repositories.CategoryRepository;
-import fr.ldnr.SpringStockMvc.repositories.CartItemRepository;
 import fr.ldnr.SpringStockMvc.repositories.UserRepository;
 import fr.ldnr.SpringStockMvc.services.CartService;
 import jakarta.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-//import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -27,63 +22,65 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ArticleController {
-    @Autowired
-    ArticleRepository articleRepository;
 
-    @Autowired
-    CategoryRepository categoryRepository;
+  @Autowired
+  ArticleRepository articleRepository;
 
-    @Autowired
-    CartItemRepository cartItemRepository;
+  @Autowired
+  CategoryRepository categoryRepository;
 
-    @Autowired
-    CartRepository cartRepository;
+  @Autowired
+  CartItemRepository cartItemRepository;
 
-    @Autowired
-    UserRepository userRepository;
+  @Autowired
+  CartRepository cartRepository;
 
-    @Autowired
-    private CartService cartService;
+  @Autowired
+  UserRepository userRepository;
 
-    //@RequestMappingValue(value="/index" , method=RequestMethod.GET)
-    @GetMapping("/index")
-    public String index(Model model, @RequestParam(name="page",defaultValue = "0") int page,
-                                     @RequestParam(name="keyword", defaultValue = "") String kw,
-         @RequestParam(name="categoryId", defaultValue = "0") long categoryId){
-      Page<Article> articles;
-      if(categoryId != 0){
-        articles = articleRepository.findByCategoryId(categoryId,PageRequest.of(page,5) );
-      } else {
-        articles = articleRepository.findByDescriptionContains(kw,
-            PageRequest.of(page, 5));
-      }
+  @Autowired
+  private CartService cartService;
 
-      List<Category> categories = categoryRepository.findAll();
-      model.addAttribute("listArticle",articles.getContent());
-      model.addAttribute("listCategories",categories);
-      model.addAttribute("pages", new int[articles.getTotalPages()]);
-      model.addAttribute("categoryId",categoryId);
-      model.addAttribute("currentPage",page);
-
-      model.addAttribute("keyword",kw);
-
-      return "articles";
+  //@RequestMappingValue(value="/index" , method=RequestMethod.GET)
+  @GetMapping({"/","/index"})
+  public String index(Model model, @RequestParam(name = "page", defaultValue = "0") int page,
+      @RequestParam(name = "keyword", defaultValue = "") String kw,
+      @RequestParam(name = "categoryId", defaultValue = "0") long categoryId) {
+    Page<Article> articles;
+    if (categoryId != 0) {
+      articles = articleRepository.findByCategoryId(categoryId, PageRequest.of(page, 5));
+    } else {
+      articles = articleRepository.findByDescriptionContains(kw,
+          PageRequest.of(page, 5));
     }
 
-    @GetMapping("/delete")
-    public String delete(Long id, int page, String keyword){
-      articleRepository.deleteById(id);
+    List<Category> categories = categoryRepository.findAll();
+    model.addAttribute("listArticle", articles.getContent());
+    model.addAttribute("listCategories", categories);
+    model.addAttribute("pages", new int[articles.getTotalPages()]);
+    model.addAttribute("categoryId", categoryId);
+    model.addAttribute("currentPage", page);
 
-      return "redirect:/index?page="+page+"&keyword="+keyword;
-    }
+    model.addAttribute("keyword", kw);
 
-    @GetMapping("/article")
-    public String article(Model model){
-      model.addAttribute("article", new Article());
-      List<Category> categories = categoryRepository.findAll();
-      model.addAttribute("listCategories",categories);
-      return "article";
-    }
+    return "articles";
+  }
+
+
+  @GetMapping("/delete")
+  public String delete(Long id, int page, String keyword) {
+    articleRepository.deleteById(id);
+
+    return "redirect:/index?page=" + page + "&keyword=" + keyword;
+  }
+
+  @GetMapping("/article")
+  public String article(Model model) {
+    model.addAttribute("article", new Article());
+    List<Category> categories = categoryRepository.findAll();
+    model.addAttribute("listCategories", categories);
+    return "article";
+  }
 
   @GetMapping("/editArticle")
   public String editArticle(@RequestParam("id") Long id, Model model) {
@@ -145,11 +142,12 @@ public class ArticleController {
   }
 
 
-
   @PostMapping("/save")
-    public String save(@Valid Article article, BindingResult bindingResult){
-      if(bindingResult.hasErrors()) return "article";
-      articleRepository.save(article);
-      return "redirect:/index";
+  public String save(@Valid Article article, BindingResult bindingResult) {
+    if (bindingResult.hasErrors()) {
+      return "article";
     }
+    articleRepository.save(article);
+    return "redirect:/index";
+  }
 }
